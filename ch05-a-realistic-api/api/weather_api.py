ebc2d211c0e0e6c453e60b7170899dc5d7845d5c
Javier Piedragil <javier.piedragil@humansite.com.mx>
2024-01-03 01:26:06 -0600

    📦 NEW: Weather API signature - Add parameters to weather_api.py

--- ch05-a-realistic-api/api/weather_api.py
+++ ch05-a-realistic-api/api/weather_api.py
@@ -1,8 +1,13 @@
 import fastapi
 
+from typing_extensions import Optional
+
 router = fastapi.APIRouter()
 
 
-@router.get("/api/weather")
-def weather():
-    return "Some report"
+@router.get("/api/weather/{city}")
+def weather(city: str,
+            state: Optional[str] = None,
+            country: str = "US",
+            units: Optional[str] = "metric"):
+    return f"{city},  {state},  {country},  {units}"
