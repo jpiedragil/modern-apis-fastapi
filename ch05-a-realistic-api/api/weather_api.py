9d2c243712ea2d3a6696a73118b6221d073954ed
Javier Piedragil <javier.piedragil@humansite.com.mx>
2024-01-03 01:49:54 -0600

    👌 IMPROVE: Pydantic models - Add models directory to hold Pydantic models. - Add location model. - Modify weather_api.py to use location model. - Add Pydantic to requirements.txt.

--- ch05-a-realistic-api/api/weather_api.py
+++ ch05-a-realistic-api/api/weather_api.py
@@ -1,13 +1,12 @@
 import fastapi
 
+from fastapi import Depends
+from models.location import Location
 from typing_extensions import Optional
 
 router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
-def weather(city: str,
-            state: Optional[str] = None,
-            country: str = "US",
-            units: Optional[str] = "metric"):
-    return f"{city},  {state},  {country},  {units}"
+def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+    return f"{loc.city},  {loc.state},  {loc.country},  {units}"
