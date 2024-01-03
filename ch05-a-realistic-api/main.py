360ab9ab04ea7b3b9eec29f8b1e2cbdeb2010b9b
Javier Piedragil <javier.piedragil@humansite.com.mx>
2024-01-03 02:58:41 -0600

    📦 NEW: Setting the API key II - Add logic to read OpenWeather API key from settings.json.

--- ch05-a-realistic-api/main.py
+++ ch05-a-realistic-api/main.py
@@ -1,22 +1,39 @@
+import json
+from pathlib import Path
+
 import fastapi
 import uvicorn
 
 from starlette.staticfiles import StaticFiles
 
 from api import weather_api
+from services import open_weather_service
 from views import home
+
 
 api = fastapi.FastAPI()
 
 
 def configure():
     configure_routing()
+    configure_api_keys()
 
 
 def configure_routing():
     api.mount("/static", StaticFiles(directory="./static"), name="static")
     api.include_router(home.router)
     api.include_router(weather_api.router)
+
+
+def configure_api_keys():
+    file = Path("settings.json").absolute()
+
+    if not file.exists():
+        print(f"WARNING: {file} not found, you cannot continue, please see settings_template.json")
+        raise Exception("settings.json file not found, you cannot continue, please see settings_template.json")
+    with open("settings.json") as fin:
+        settings = json.load(fin)
+        open_weather_service.api_key = settings.get("api_key")
 
 
 if __name__ == "__main__":
--- ch05-a-realistic-api/services/open_weather_service.py
+++ ch05-a-realistic-api/services/open_weather_service.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+api_key: Optional[str] = None
+
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
     q = f"{city}, {country}"
